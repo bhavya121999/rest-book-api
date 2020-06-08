d01a329fa25ed95a9c4582756c53ba93d24c1bb1
bhavya121999 <bhavyagupta1203@gmail.com>
2020-06-08 22:53:13 +0530

    Created submit form

--- src/main/webapp/reactjs/src/components/Book.js
+++ src/main/webapp/reactjs/src/components/Book.js
@@ -1,13 +1,129 @@
-import React from "react";
+import React,{Component} from "react";
+import {Card,Form,Button,Col} from "react-bootstrap"
 
-class Book extends React.Component {
-	render(){
-		return (
-			<div className="text-white">Add Book</div>
-			
+export default class Book extends Component {
 
-			);
-	}
+constructor(props){
+	super(props);
+	this.state = {title:"",author:"",coverphotoURL:"",isbnNumber:"",price:"",language:""};
+	this.bookChange=this.bookChange.bind(this);
+	this.submitBook=this.submitBook.bind(this);
+
 }
 
-export default Book;
+submitBook(event){
+	// alert(Title: "+this.state.title+", 
+	// 	Author: "+this.state.author+", 
+	// 	Cover Photo URL: "+this.state.coverphotoURL+", 
+	// 	ISBN Number: "this.state.isbnNumber+", 
+	// 	Price: "+this.state.price+", 
+	// 	Language: "+this.state.language+");
+	event.preventDefault();
+}
+
+bookChange(event){
+	this.setState({
+		[event.target.name]:event.target.name
+	})
+}
+
+render(){
+	return (
+		<Card className={"border border-dark bg-dark text-white"}>
+		<Card.Header>Add New Book</Card.Header>
+		<Form onSubmit={this.submitBook} id="bookFormId">
+		
+		<Card.Body>
+		<Form.Row>
+		<Form.Group as= {Col} controlId="formGridTitle">
+		<Form.Label>Title</Form.Label>
+		<Form.Control required 
+		type="test" 
+		name="title" 
+		value={this.state.title} 
+		onChange={this.bookChange} 
+		className={"bg-dark text-white"} 
+		placeholder="Enter Book Title" />
+
+		</Form.Group>
+		
+		<Form.Group as= {Col} controlId="formGridAuthor">
+		<Form.Label>Author</Form.Label>
+		<Form.Control type="test" 
+		name="author" 
+		value={this.state.author} 
+		onChange={this.bookChange} 
+		className={"bg-dark text-white"} 
+		placeholder="Enter Book Author" />
+
+		</Form.Group>
+        </Form.Row>
+		
+
+		<Form.Row>
+		<Form.Group as= {Col} controlId="formGridisbnNumber">
+		<Form.Label>ISBN Number</Form.Label>
+		<Form.Control type="test" 
+		name="isbnNumber" 
+		value={this.state.isbnNumber} 
+		onChange={this.bookChange} 
+		className={"bg-dark text-white"} 
+		placeholder="Enter Book ISBN Number" />
+
+		</Form.Group>
+		<Form.Group as= {Col} controlId="formGridcoverphotoURL">
+		<Form.Label>Cover Photo URL</Form.Label>
+		<Form.Control 
+		type="test" 
+		name="coverphotoURL" 
+		value={this.state.coverphotoURL} 
+		onChange={this.bookChange} 
+		className={"bg-dark text-white"} 
+		placeholder="Enter Book Cover Photo URL" />
+
+		</Form.Group>
+        </Form.Row>
+		
+		
+		<Form.Row>
+		<Form.Group as= {Col} controlId="formGridprice">
+		<Form.Label>Price</Form.Label>
+		<Form.Control 
+		type="test" 
+		name="price" 
+		value={this.state.price} 
+		onChange={this.bookChange} 
+		className={"bg-dark text-white"} 
+		placeholder="Enter Book Price" />
+
+		</Form.Group>
+		<Form.Group as= {Col} controlId="formGridlanguage">
+		<Form.Label>Language</Form.Label>
+		<Form.Control 
+		type="test" 
+		name="language" 
+		value={this.state.language} 
+		onChange={this.bookChange} 
+		className={"bg-dark text-white"} 
+		placeholder="Enter Book Language" />
+
+		</Form.Group>
+        </Form.Row>
+
+
+        
+        <Card.Footer style={{"textAlign":"right"}}>
+		<Button size="sm" variant="primary" type="submit">
+		Submit
+		</Button>
+		</Card.Footer>
+
+		</Card.Body>
+		</Form>
+		</Card>
+		
+
+		);
+}
+}
+
--- src/main/webapp/reactjs/src/components/BookList.js
+++ src/main/webapp/reactjs/src/components/BookList.js
@@ -1,9 +1,33 @@
 import React from "react";
+import {Card,Table} from "react-bootstrap"
 
 class BookList extends React.Component {
 	render(){
 		return (
-			<div className="text-white">Book List</div>
+			<Card className={"border border-dark bg-dark text-white"}>
+			<Card.Header>Book List</Card.Header>
+			<Card.Body>
+			<Table bordered hover striped variant="dark">
+			<thead>
+    <tr>
+      <th>Title</th>
+      <th>Author</th>
+      <th>ISBN Number</th>
+      <th>Price</th>
+      <th>Language</th>
+      <th>Actions</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr align="center">
+      <td colspan="6">No Books Available</td>
+      
+    </tr>
+    
+  </tbody>
+			</Table>
+			</Card.Body>
+			</Card>
 
 
 			)
